408a0d1965c660d3f75ae2cb402302057e6ecb16
Vik Boyechko <vik.boyechko@gmail.com>
2024-02-14 20:47:23 -0600

    added Comments model and index relationships to Users and Posts

---
+++ models/Comments.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create Comments model and datatypes, including the user_id foreign key.
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments',
+    }
+);
+
+module.exports = Comments;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Users = require('./Users');
 const Posts = require('./Posts');
+const Comments = require('./Comments');
 
 // Creates a relationship between Users and Posts model, with the Users having a "has many" relationship with Posts model.
 Users.hasMany(Posts, {
@@ -12,4 +13,26 @@ Posts.belongsTo(Users, {
     foreignKey: 'user_id',
 });
 
-module.exports = { Users, Posts };
+// Creates a relationship between Users and Comments model, with the Users having a "has many" relationship with Comments model.
+Users.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Creates a relationship between Users and Comments model, with a "belongs to" relationship of the Comments to the Users.
+Comments.belongsTo(Users, {
+    foreignKey: 'user_id',
+});
+
+// Creates a relationship between Posts and Comments model, with the Posts having a "has many" relationship with Comments model.
+Posts.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+// Creates a relationship between Posts and Comments model, with a "belongs to" relationship of the Comments to the Posts.
+Comments.belongsTo(Posts, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { Users, Posts, Comments };
